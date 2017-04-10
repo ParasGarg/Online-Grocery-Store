3ef27be5ecf4b2a9ee1f84a24885ee69da46d872
Paras Garg <pgarg2@stevens.edu>
2017-04-10 13:53:42 -0400

    credential access on admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,17 @@
  * Admin Routers *
  * This route file contains all apis for admin operations
  * Functionalities Index: 
-        ==============================================================================================
-        | S.No. |  Type  |        URL       |   Function Call   | Fn Path |        Description       |
-        ==============================================================================================
-        |   1.  | Get    | /admin/users/    | getAllUsers       | users   | Search user from it's id |
-        ----------------------------------------------------------------------------------------------
-        |   2.  | Delete | /admin/users/:id | deleteUser        | users   | Insert new user record   |
-        ----------------------------------------------------------------------------------------------
+        ========================================================================================================
+        | S.No. |  Type  |         URL        |   Function Call   |    Fn Path    |         Description        |
+        ========================================================================================================
+        |   1.  | Get    | /admin/users/      | getUserById       | users         | Search user from it's id   |
+        --------------------------------------------------------------------------------------------------------
+        |   2.  | Get    | /admin/users/list  | getAllUsers       | users         | Search all users           |
+        --------------------------------------------------------------------------------------------------------
+        |   3.  | Delete | /admin/users/:id   | deleteUser        | users         | Delete a user record       |
+        --------------------------------------------------------------------------------------------------------
+        |   4.  | Delete | /admin/creds/:id   | deleteCredential  | credentials   | Delete a credential record |
+        --------------------------------------------------------------------------------------------------------
 */
 
 /* importing required files and packages */
@@ -16,6 +20,7 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data');
 const usersData = data.users;
+const credentialsData = data.credentails;
 
 /* routes for user collection operations */
 // route to fetch user information by id
@@ -46,6 +51,19 @@ router.get('/user/list', (req, res) => {
 router.delete('/user/id/:id', (req, res) => {
     usersData.getUserById(req.params.id).then(() => {
         usersData.deleteUser(req.params.id).then(() => {
+            res.status(200).send(`Pharse of id ${req.params.id} has been deleted`);
+        }, (err) => {
+            res.status(500).json({ error: err });            
+        });
+    }). catch(() => {
+        res.status(404).json({ error: "No such user exists" });
+    });
+});
+
+// route to delete credential information by id
+router.delete('/user/creds/:id', (req, res) => {
+    credentialsData.getCredentialById(req.params.id).then(() => {
+        credentialsData.deleteCredential(req.params.id).then(() => {
             res.status(200).send(`Pharse of id ${req.params.id} has been deleted`);
         }, (err) => {
             res.status(500).json({ error: err });            
